e770b831b91d50deea20d390587686b168c6fb4e
Christia T <christiatidwell@bellsouth.net>
2018-08-15 02:49:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ attr_accessor :id, :name, :grade
       INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
-    DB[:conn].execute(sql, self.name, self.album)
+    DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
 end
