df90c45a197d922987bac543a063697b5e433686
Christia T <christiatidwell@bellsouth.net>
2018-08-15 02:35:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,9 +6,10 @@ attr_accessor :id, :name, :grade
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-def initialize(id=nil, name, grade)
-  @id = id 
-  @name = name 
-  @grade = grade
-end
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
 end
