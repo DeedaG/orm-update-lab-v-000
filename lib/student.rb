acc4b2dda55bb283304768d4e032e17ce1174a3f
Christia T <christiatidwell@bellsouth.net>
2018-08-15 03:09:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,7 @@ attr_accessor :id, :name, :grade
     new_student = self.new
   new_student.id = row[0]
   new_student.name =  row[1]
-  new_student.length = row[2]
+  new_student.grade = row[2]
   new_student
   end
 
