1ed9ca2038016df2e6e99f66c97f093f164d8b0b
Christia T <christiatidwell@bellsouth.net>
2018-08-15 02:57:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,6 +49,7 @@ attr_accessor :id, :name, :grade
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
     Song.new(result[0], result[1], result[2])
+  end
 
 
   def update
